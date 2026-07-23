090e3e9bae7d849b037b27ad52339e620b2766eb
arashime0606-sys <arashime0606@gmail.com>
2026-07-24 00:55:42 +0900

    Update fortune.js

--- api/fortune.js
+++ api/fortune.js
@@ -11,7 +11,7 @@ function isRateLimited(ip) {
   return timestamps.length > RATE_LIMIT_MAX;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
